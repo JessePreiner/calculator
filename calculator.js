e289a68fac39cd438cbaf3e5ba34561bf98c578a
Jesse Preiner <jezzah@gmail.com>
2016-08-26 16:53:13 -0600

    Map response of length width and depth to an object

--- calculator.js
+++ calculator.js
@@ -3,7 +3,35 @@ $(document).ready(function() {
   let actions = areaWrap.find('.area-actions');
 
   const calculate = () => {
-    alert('calculating');
+    // store each area separately
+    let total = 0;
+    let areas = $('.area');
+
+    areas.each((idx, area)=> {
+      let jArea = $.makeArray($(area));
+      let test = jArea.map((area) => {
+        let length = $(area).find('.input-length');
+        let width = $(area).find('.input-width');
+        let depth = $(area).find('.input-depth');
+        return {
+          'length': {
+            'ft': length.eq(0).val(),
+            'in': length.eq(1).val(),
+          },
+          'width':  {
+            'ft': width.eq(0).val(),
+            'in': width.eq(1).val(),
+          },
+          'depth':  {
+            'ft': depth.eq(0).val(),
+            'in': depth.eq(1).val(),
+          }
+        }
+      });
+
+      console.log(test);
+
+    });
   }
 
   const setupButtons = (actions) => {
